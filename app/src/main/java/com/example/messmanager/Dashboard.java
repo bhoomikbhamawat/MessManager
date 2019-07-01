f8ba48cecce291a2e134af728221c5bc31816322
bhoomikbhamawat <bhoomikbhamawat.eee18@itbhu.ac.in>
2019-07-01 10:26:21 +0530

    Created AttendanceActivity

--- app/src/main/java/com/example/messmanager/Dashboard.java
+++ app/src/main/java/com/example/messmanager/Dashboard.java
@@ -1,14 +1,29 @@
 package com.example.messmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Dashboard extends AppCompatActivity {
+
+    private CardView attendance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        attendance=findViewById(R.id.attendance_cardview);
+
+        attendance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Dashboard.this,AttendanceActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
